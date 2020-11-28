b57a71a84cf68eee286c25a7e4d0510a4a62191f
yuyeonee <63159699+yuyeonee@users.noreply.github.com>
2020-11-28 22:27:03 +0900

    integrate twohands

--- YamiTetris/tetris/Tetris.py
+++ YamiTetris/tetris/Tetris.py
@@ -107,7 +107,7 @@ class Tetris:
                     screen = pygame.display.set_mode((event.w, event.h),pygame.RESIZABLE )
 
             # self.screen.fill(BLACK)
-            self.board.draw(self)
+            self.board.draw(self, self.mode)
             pygame.display.update() #이게 나오면 구현 시
             self.clock.tick(30) # 초당 프레임 관련
 
@@ -132,7 +132,7 @@ class Mini:
     #각 키를 누를떄 실행되는 method
     def handle_key(self, event_key, mode):
         if event_key == K_DOWN or event_key == K_s:
-            self.board.drop_piece()
+            self.board.drop_piece(self.mode)
         elif event_key == K_LEFT or event_key == K_a:
             self.board.move_piece(dx=-1, dy=0)
         elif event_key == K_RIGHT or event_key == K_d:
@@ -219,6 +219,6 @@ class Mini:
 
 
             # self.screen.fill(BLACK)
-            self.board.draw(self)
+            self.board.draw(self, self.mode)
             pygame.display.update() #이게 나오면 구현 시
             self.clock.tick(30) # 초당 프레임 관련
